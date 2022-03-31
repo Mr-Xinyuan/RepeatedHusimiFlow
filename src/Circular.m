1b342df549f9ea359e7bc668cc8fb5248fe7b7ac
Mr_Xinyuan <2271280806@qq.com>
2022-03-31 13:03:23 +0800

    fix confident

---
+++ src/Circular.m
@@ -0,0 +1,10 @@
+function [res, r] = Circular(rlim, N, V)
+%Circular - Description
+%
+% Syntax: [res, r] = Circular(rlim, N, V)
+%
+% calculate the circular well in V potential
+
+
+
+end
--- src/PsiA.m
+++ src/PsiA.m
@@ -12,15 +12,14 @@ function [res, x, y] = PsiA(k, sigma, Nx, Ny, disp)
         shading interp 
         view(2)
 
-        xlim([-sigma, sigma]);
-        ylim([-sigma, sigma]);
-        set(gca,'xtick', [-floor(sigma) 0 floor(sigma)]);
-        set(gca,'ytick', [-floor(sigma) 0 floor(sigma)]);
+        axis([-sigma, sigma -sigma, sigma])
+        %set(gca,'xtick', [-floor(sigma) 0 floor(sigma)]);
+        %set(gca,'ytick', [-floor(sigma) 0 floor(sigma)]);
         %set(gca,'xtick', [-0.4 0 0.4]);
         %set(gca,'ytick', [-0.4 0 0.4]);
-        set(gca,'FontSize',25)
-        set(gcf,'unit','centimeters','position',[0 0 10 10]);
-        filename = ['../figure/PsiA' num2str(1/sigma*100) '.png'];
-        saveas(gcf, filename, 'png');
+        %set(gca,'FontSize',25)
+        %set(gcf,'unit','centimeters','position',[0 0 10 10]);
+        %filename = ['../figure/PsiA' num2str(1/sigma*100) '.png'];
+        %saveas(gcf, filename, 'png');
     end
 end
